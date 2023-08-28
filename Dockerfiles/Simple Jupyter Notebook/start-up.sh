c1218058465e488c3748583b97ca9a084ba2e073
Wes Carroll <124593603+wcarrollrai@users.noreply.github.com>
2023-08-28 16:44:45 -0400

    Added comments to sample files

--- Dockerfiles/Simple Jupyter Notebook/start-up.sh
+++ Dockerfiles/Simple Jupyter Notebook/start-up.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Clone a GIT repo into the running container into a specific directory
 git clone https://github.com/wcarrollrai/containers-demo /home/jovyan/work
 
+# Install any python packages cloned from the repo
 pip install -r /home/jovyan/work/requirements.txt
 
+# Run Jupyter lab and allow for RunAI added parameters
 jupyter lab --port=8888 --ip=0.0.0.0 --allow-root $1 $2 $3
